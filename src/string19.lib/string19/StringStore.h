2670a82d8f5ea1c2abefdbdf7ff1efaeaaa9c51f
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-05-30 00:38:58 +0200

    prefer enum to constexpr auto - it's shorter and simpler to read

--- src/string19.lib/string19/StringStore.h
+++ src/string19.lib/string19/StringStore.h
@@ -11,13 +11,13 @@ template<size_t N> using CharArray = char[N];
 /// note: the stored array is always 0 terminated.
 /// also note: N is the number of actual characters (not including the trailing \0)
 template<size_t N> struct StringStore {
+    enum { count = N };
     CharArray<N + 1> data{};
-    static constexpr auto count = N;
 
     constexpr char operator[](size_t i) const noexcept { return data[i]; }
 };
 
 /// note: always use storeLiteral("Hello") - or you get a 2nd \0 into your string!
-template<size_t N> StringStore(const char (&)[N]) -> StringStore<N>;
+template<size_t N> StringStore(const char (&)[N])->StringStore<N>;
 
 } // namespace string19
