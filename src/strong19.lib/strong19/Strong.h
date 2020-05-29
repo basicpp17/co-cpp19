b0c4ed7c306e894a6cb5d8c1d24504f397834243
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-05-30 00:55:57 +0200

    moved to private base for strong to allow std::string as value type

--- src/strong19.lib/strong19/Strong.h
+++ src/strong19.lib/strong19/Strong.h
@@ -13,22 +13,25 @@ template<class T> constexpr inline auto is_strong = isStrong(nullptr_to<ADL>, nu
 /// view to the name of the strong type T (is_strong<T> == true)
 template<class T> constexpr inline auto strong_name = strongName(nullptr_to<T>);
 
-/// Weak base class for a strong type
-/// WARNING: using this defeats the purpose of the strong type!
-/// - used as fallback in ostream!
-template<class V> struct Weak { constexpr auto operator<=>(const Weak&) const = default; };
+/// private base class for a strong type
+/// note: required or constexpr would be an error for non-constexpr types (like std::string)
+template<class V> struct Weak {
+    static_assert(!is_strong<V>);
+    V v;
+    constexpr Weak() = default;
+    constexpr explicit Weak(V v) : v(v) {}
+    constexpr auto operator<=>(const Weak&) const = default;
+};
 
 #ifndef DEFINE_STRONG_EXTRAS
 #    define DEFINE_STRONG_EXTRAS(NAME)
 #endif
 
-// defines a strong value type
+/// Defines a strong value type with name @param NAME that stores value of type @param VALUE and optional tags
 #define DEFINE_STRONG(NAME, VALUE, ...)                                                                                \
-    struct NAME {                                                                                                      \
-        static_assert(!is_strong<VALUE>);                                                                              \
-        VALUE v;                                                                                                       \
-        constexpr NAME() = default;                                                                                    \
-        constexpr explicit NAME(VALUE v) : v(v) {}                                                                     \
+    struct NAME : private strong19::Weak<VALUE> {                                                                      \
+        using Weak::Weak;                                                                                              \
+        using Weak::v;                                                                                                 \
         constexpr auto operator<=>(const NAME&) const = default;                                                       \
     };                                                                                                                 \
     constexpr inline auto isStrong(strong19::ADL*, NAME*)->bool { return true; }                                       \
@@ -38,21 +41,9 @@ template<class V> struct Weak { constexpr auto operator<=>(const Weak&) const = 
     DEFINE_STRONG_EXTRAS(NAME)                                                                                         \
     struct NAME
 
-// defines an abstract strong type
-#define DEFINE_ABSTRACT_STRONG(NAME, ABSTRACT, ...)                                                                    \
-    struct NAME;                                                                                                       \
-    constexpr inline auto isStrong(strong19::ADL*, NAME*)->bool { return true; }                                       \
-    auto strongType(NAME*)->ABSTRACT;                                                                                  \
-    auto strongTags(NAME*)->meta19::TypePack<##__VA_ARGS__>;                                                           \
-    constexpr inline auto strongName(NAME*)->string19::StringView { return string19::viewLiteral(#NAME); }             \
-    struct NAME
-
 /// Fallback for any type that does not have isStrong overload
 /// note: This overload is found through argument dependent lookup of strong19::ADL
 constexpr inline auto isStrong(ADL*, void*) -> bool { return false; }
-// template<class T> auto strongName(T*);
-// template<class T> auto strongValueType(T*);
-// template<class T> auto strongTags(T*);
 
 /// stored type of the strong type T (is_strong<T> == true)
 template<class T> using StrongValueType = decltype(strongValueType(nullptr_to<T>));
