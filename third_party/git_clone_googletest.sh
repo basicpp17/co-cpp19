ca7f573a834a1c23e5b2f34d3875f3f0bc4c2a60
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-06-11 12:44:35 +0200

    update googletest

--- third_party/git_clone_googletest.sh
+++ third_party/git_clone_googletest.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-git clone -b release-1.10.0 --depth 1 --no-tags https://github.com/google/googletest.git "$(dirname "$0")/googletest"
+if [ ! -d "$(dirname "$0")/googletest" ]; then
+    git clone --depth 1 --no-tags https://github.com/google/googletest.git "$(dirname "$0")/googletest"
+fi
+pushd "$(dirname "$0")/googletest"
+git fetch origin 210aab09deef06e64ab82414697abd0d7cb5978a
+git reset --hard FETCH_HEAD
+popd
