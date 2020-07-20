0e577ab8df2a739c04391c11cfb9c47fd1bdf2b4
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-07-20 12:56:06 +0200

    Deduction guide for EnumName wrapper

--- src/enum19.lib/enum19/Enum.ostream.h
+++ src/enum19.lib/enum19/Enum.ostream.h
@@ -9,6 +9,8 @@ namespace enum19 {
 /// This wrapper enables ADL for ostream operator
 template<class T> requires(std::is_enum_v<T>) struct EnumName { T v; };
 
+template<class T> EnumName(T) -> EnumName<T>;
+
 template<class T> auto operator<<(std::ostream& out, EnumName<T> wrapper) -> std::ostream& {
     auto underlying = static_cast<std::underlying_type_t<T>>(wrapper.v);
     return out << valueName(wrapper.v) << " (" << underlying << ")";
