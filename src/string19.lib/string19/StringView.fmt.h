f24f2ce03c77a661457d26db85fc9f169c76c0be
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-07-28 20:00:58 +0200

    fixed fmt deprecation warning

--- src/string19.lib/string19/StringView.fmt.h
+++ src/string19.lib/string19/StringView.fmt.h
@@ -11,7 +11,7 @@ template<class char_type> struct formatter<string19::StringView, char_type> {
     constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
 
     template<typename FormatCtx> auto format(const string19::StringView& v, FormatCtx& ctx) {
-        auto out = ctx.begin();
+        auto out = ctx.out();
         for (auto i = 0u; i < v.count; i++) *out++ = v[i];
         return out;
     }
