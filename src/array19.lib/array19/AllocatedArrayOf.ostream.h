3071db312b8abd5a3baf455026633cc9d120013f
Andreas Reischuck <andreas.reischuck@hicknhack-software.com>
2020-08-31 11:11:18 +0200

    Fixed copy paste issue

--- src/array19.lib/array19/AllocatedArrayOf.ostream.h
+++ src/array19.lib/array19/AllocatedArrayOf.ostream.h
@@ -1,12 +1,12 @@
 #pragma once
-#include "DynamicArrayOf.h"
+#include "AllocatedArrayOf.h"
 
 #include <ostream>
 
 namespace array19 {
 
 template<class Chr, class Traits, class T>
-auto operator<<(std::basic_ostream<Chr, Traits>& out, const DynamicArrayOf<T>& a) -> decltype(out)& {
+auto operator<<(std::basic_ostream<Chr, Traits>& out, const AllocatedArrayOf<T>& a) -> decltype(out)& {
     out << "[";
     bool first = true;
     for (auto& v : a) {
