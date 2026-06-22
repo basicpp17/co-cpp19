120c41fe5042d206c86f4613ce6c64b9e19c555b
Christian von Wascinski <harryherold@googlemail.com>
2026-06-22 09:45:50 +0200

    fix(string19): Disable range handling.

--- src/string19.lib/string19/StringView.fmt.h
+++ src/string19.lib/string19/StringView.fmt.h
@@ -2,7 +2,16 @@
 #include "StringView.h"
 
 /// note: for this header you need fmt library (not included as library dependency)
-#include <fmt/format.h>
+#include <fmt/format.h> // oder <fmt/base.h>
+#include <fmt/ranges.h> // muss vor deiner Spezialisierung sein
+
+namespace fmt {
+
+template<typename Char> struct range_format_kind<string19::StringView, Char> {
+    static constexpr auto value = range_format::disabled;
+};
+
+} // namespace fmt
 
 template<class Char> struct fmt::formatter<string19::StringView, Char> {
     constexpr auto parse(fmt::basic_format_parse_context<Char>& ctx) { return ctx.begin(); }
